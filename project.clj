2af452993359d7de75bf08c0d4978a2bfa46b5a3
nha <nha@users.noreply.github.com>
2016-10-06 00:47:27 +0100

    chore: update marginalia version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject fogus/minderbinder "0.3.0-SNAPSHOT"
   :description "Converting one thing into another thing via Clojure at read time and compile time."
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]]
-  :plugins [[marginalia "0.8.0-SNAPSHOT"]]
+  :plugins [[marginalia "0.9.0"]]
   :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]])
