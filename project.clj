76e887f758182910f802da190bcef27f8fda2588
nha <nha@users.noreply.github.com>
2016-10-15 00:29:59 +0100

    chore: rename project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fogus/minderbinder "0.3.0-SNAPSHOT"
+(defproject nha/minderbinder "0.3.0-SNAPSHOT"
   :description "Converting one thing into another thing via Clojure at read time and compile time."
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]]
   :plugins [[marginalia "0.9.0"]]
